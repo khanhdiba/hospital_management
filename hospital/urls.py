5ab1e2b01495cd2677e0c1e02313fe6534832643
khanhdiba <dinhbakhanh1508@gmail.com>
2024-11-27 14:53:05 +0700

    Updated

---
+++ hospital/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main_page, name='main_page'),
+    path('patientclick', views.patient_click_page),
+    path('hospitalstaffclick', views.hos_staff_click_page),
+    path('hospitalstaffclick/adminclick', views.admin_click_page),
+    path('hospitalstaffclick/recepclick', views.recep_click_page),
+    path('hospitalstaffclick/doctorclick', views.doctor_click_page),
+    path('hospitalstaffclick/nurseclick', views.nurse_click_page),
+]
---
+++ hospital/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+# def members(request):
+#   template = loader.get_template('myfirst.html')
+#   return HttpResponse(template.render())
+
+def main_page(request):
+  return render(request, 'main_page.html')
+
+def patient_click_page(request):
+  return render(request, 'patientclick.html')
+
+def hos_staff_click_page(request):
+  return render(request, 'hospitalstaffclick.html')
+
+def admin_click_page(request):
+  return render(request, 'adminclick.html')
+
+def recep_click_page(request):
+  return render(request, 'recepclick.html')
+
+def doctor_click_page(request):
+  return render(request, 'doctorclick.html')
+
+def nurse_click_page(request):
+  return render(request, 'nurseclick.html')
+
